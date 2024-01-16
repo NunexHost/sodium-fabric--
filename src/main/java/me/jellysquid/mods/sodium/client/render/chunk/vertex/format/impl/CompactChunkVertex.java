0226c390f93b6cd8529449a51ab19943b6c14159
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-15 21:44:45 -0300

    Update CompactChunkVertex.java

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/vertex/format/impl/CompactChunkVertex.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/vertex/format/impl/CompactChunkVertex.java
@@ -1,29 +1,23 @@
 package me.jellysquid.mods.sodium.client.render.chunk.vertex.format.impl;
 
-import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexAttributeFormat;
-import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexFormat;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import com.mojang.blaze3d.vertex.VertexFormatElement;
 import me.jellysquid.mods.sodium.client.render.chunk.terrain.material.Material;
 import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkMeshAttribute;
 import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkVertexEncoder;
-import me.jellysquid.mods.sodium.api.util.ColorABGR;
-import me.jellysquid.mods.sodium.api.util.ColorU8;
-import org.lwjgl.system.MemoryUtil;
 
 public class CompactChunkVertex implements ChunkVertexType {
-    public static final GlVertexFormat<ChunkMeshAttribute> VERTEX_FORMAT = GlVertexFormat.builder(ChunkMeshAttribute.class, 16)
-            .addElement(ChunkMeshAttribute.VERTEX_DATA, 0, GlVertexAttributeFormat.UNSIGNED_INT, 4, false, true)
+    public static final VertexFormat<ChunkMeshAttribute> VERTEX_FORMAT = VertexFormat.builder(ChunkMeshAttribute.class)
+            .addElement(ChunkMeshAttribute.VERTEX_DATA, 0, VertexFormatElement.Type.FLOAT, 4, false, true)
             .build();
 
-    public static final int STRIDE = 16;
-
-    private static final int POSITION_MAX_VALUE = 65536;
-    private static final int TEXTURE_MAX_VALUE = 32768;
+    private static final int STRIDE = 16;
 
     private static final float MODEL_ORIGIN = 8.0f;
     private static final float MODEL_SCALE = 32.0f;
 
     @Override
-    public GlVertexFormat<ChunkMeshAttribute> getVertexFormat() {
+    public VertexFormat<ChunkMeshAttribute> getVertexFormat() {
         return VERTEX_FORMAT;
     }
 
@@ -31,15 +25,15 @@ public class CompactChunkVertex implements ChunkVertexType {
     public ChunkVertexEncoder getEncoder() {
         return (ptr, material, vertex, sectionIndex) -> {
             // Positions
-            int position = encodePosition(vertex.x) | (encodePosition(vertex.y) << 16);
-            int drawParameters = encodeDrawParameters(material, sectionIndex);
+            int position = (int) ((vertex.x - MODEL_ORIGIN) * MODEL_SCALE);
+            int drawParameters = (sectionIndex & 0xFF) << 8 | (material.bits() & 0xFF);
 
             // Colors
-            int color = encodeColor(vertex.color);
-            int light = encodeLight(vertex.light);
+            int color = ColorABGR.pack(vertex.color);
+            int light = (vertex.light >> 4) & 0xF;
 
             // Texture
-            int texture = encodeTexture(vertex.u, vertex.v);
+            int texture = (int) (vertex.u * 32768.0f) | ((int) (vertex.v * 32768.0f) << 16);
 
             // Write to buffer
             MemoryUtil.memPutInt(ptr, position | drawParameters);
@@ -51,31 +45,23 @@ public class CompactChunkVertex implements ChunkVertexType {
     }
 
     private static int encodePosition(float value) {
-        // Pre-calculate constant
-        final float scale = POSITION_MAX_VALUE / MODEL_SCALE;
-
-        return (int) ((MODEL_ORIGIN + value) * scale);
+        return (int) (value * MODEL_SCALE);
     }
 
     private static int encodeDrawParameters(Material material, int sectionIndex) {
-        // Combine encoding steps
-        return ((sectionIndex & 0xFF) << 8) | ((material.bits() & 0xFF) << 0);
+        return (sectionIndex & 0xFF) << 8 | (material.bits() & 0xFF);
     }
 
     private static int encodeColor(int color) {
-        // Use half-precision floats
-        return ColorU8.byteToFloat(ColorABGR.unpackAlpha(color)) * 0xFFFF;
+        return ColorABGR.pack(color);
     }
 
     private static int encodeLight(int light) {
-        // Pack color and light data into a single byte
-        return ((light >> 4) & 0xF) | ((light >> 20) & 0xF);
+        return (light >> 4) & 0xF;
     }
 
     private static int encodeTexture(float u, float v) {
-        // Use integer rounding
-        return ((Math.round(u * TEXTURE_MAX_VALUE) & 0xFFFF) << 0) |
-                ((Math.round(v * TEXTURE_MAX_VALUE) & 0xFFFF) << 16);
+        return (int) (u * 32768.0f) | ((int) (v * 32768.0f) << 16);
     }
-        }
-            
+    }
+                                 
