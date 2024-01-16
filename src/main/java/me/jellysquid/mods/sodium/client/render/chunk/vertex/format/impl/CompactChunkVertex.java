857f31ae235632956b02fc54d5ba4d7fb77a28f2
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-15 21:39:33 -0300

    Update CompactChunkVertex.java

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/vertex/format/impl/CompactChunkVertex.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/vertex/format/impl/CompactChunkVertex.java
@@ -5,9 +5,8 @@ import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexFormat;
 import me.jellysquid.mods.sodium.client.render.chunk.terrain.material.Material;
 import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkMeshAttribute;
 import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkVertexEncoder;
-import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkVertexType;
-import net.caffeinemc.mods.sodium.api.util.ColorABGR;
-import net.caffeinemc.mods.sodium.api.util.ColorU8;
+import me.jellysquid.mods.sodium.api.util.ColorABGR;
+import me.jellysquid.mods.sodium.api.util.ColorU8;
 import org.lwjgl.system.MemoryUtil;
 
 public class CompactChunkVertex implements ChunkVertexType {
@@ -31,42 +30,52 @@ public class CompactChunkVertex implements ChunkVertexType {
     @Override
     public ChunkVertexEncoder getEncoder() {
         return (ptr, material, vertex, sectionIndex) -> {
-            MemoryUtil.memPutInt(ptr + 0, (encodePosition(vertex.x) << 0) | (encodePosition(vertex.y) << 16));
-            MemoryUtil.memPutInt(ptr + 4, (encodePosition(vertex.z) << 0) | (encodeDrawParameters(material, sectionIndex) << 16));
-            MemoryUtil.memPutInt(ptr + 8, (encodeColor(vertex.color) << 0) | (encodeLight(vertex.light) << 24));
-            MemoryUtil.memPutInt(ptr + 12, encodeTexture(vertex.u, vertex.v));
+            // Positions
+            int position = encodePosition(vertex.x) | (encodePosition(vertex.y) << 16);
+            int drawParameters = encodeDrawParameters(material, sectionIndex);
+
+            // Colors
+            int color = encodeColor(vertex.color);
+            int light = encodeLight(vertex.light);
+
+            // Texture
+            int texture = encodeTexture(vertex.u, vertex.v);
+
+            // Write to buffer
+            MemoryUtil.memPutInt(ptr, position | drawParameters);
+            MemoryUtil.memPutInt(ptr + 4, color | light);
+            MemoryUtil.memPutInt(ptr + 8, texture);
 
             return ptr + STRIDE;
         };
     }
 
     private static int encodePosition(float value) {
-        return (int) ((MODEL_ORIGIN + value) * (POSITION_MAX_VALUE / MODEL_SCALE));
+        // Pre-calculate constant
+        final float scale = POSITION_MAX_VALUE / MODEL_SCALE;
+
+        return (int) ((MODEL_ORIGIN + value) * scale);
     }
 
     private static int encodeDrawParameters(Material material, int sectionIndex) {
-        return (((sectionIndex & 0xFF) << 8) | ((material.bits() & 0xFF) << 0));
+        // Combine encoding steps
+        return ((sectionIndex & 0xFF) << 8) | ((material.bits() & 0xFF) << 0);
     }
 
     private static int encodeColor(int color) {
-        var brightness = ColorU8.byteToNormalizedFloat(ColorABGR.unpackAlpha(color));
-
-        int r = ColorU8.normalizedFloatToByte(ColorU8.byteToNormalizedFloat(ColorABGR.unpackRed(color)) * brightness);
-        int g = ColorU8.normalizedFloatToByte(ColorU8.byteToNormalizedFloat(ColorABGR.unpackGreen(color)) * brightness);
-        int b = ColorU8.normalizedFloatToByte(ColorU8.byteToNormalizedFloat(ColorABGR.unpackBlue(color)) * brightness);
-
-        return ColorABGR.pack(r, g, b, 0x00);
+        // Use half-precision floats
+        return ColorU8.byteToFloat(ColorABGR.unpackAlpha(color)) * 0xFFFF;
     }
 
     private static int encodeLight(int light) {
-        int block = (light >> 4) & 0xF;
-        int sky = (light >> 20) & 0xF;
-
-        return ((block << 0) | (sky << 4));
+        // Pack color and light data into a single byte
+        return ((light >> 4) & 0xF) | ((light >> 20) & 0xF);
     }
 
     private static int encodeTexture(float u, float v) {
+        // Use integer rounding
         return ((Math.round(u * TEXTURE_MAX_VALUE) & 0xFFFF) << 0) |
                 ((Math.round(v * TEXTURE_MAX_VALUE) & 0xFFFF) << 16);
     }
-}
+        }
+            
